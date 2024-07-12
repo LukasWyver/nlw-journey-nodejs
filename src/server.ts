8ab802103a52411661fa482395c51ed2f4b418cf
lukaswyver <lucas_sin5@hotmail.com>
2024-07-11 23:12:23 -0300

    Aula 03 - Rotas da API finalizadas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { getLinks } from './routes/get-links';
 import { createTrip } from './routes/create-trip'
+import { updateTrip } from './routes/update-trip';
+import { createLink } from './routes/create-link';
 import { confirmTrip } from './routes/confirm-trip';
+import { createInvite } from './routes/create-invite';
+import { getActivities } from './routes/get-activities';
+import { createActivity } from './routes/create-activity';
+import { getParticipants } from './routes/get-participants';
 import { confirmParticipant } from './routes/confirm-participant';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { createActivity } from './routes/create-activity';
-import { getActivities } from './routes/get-activities';
-import { createLink } from './routes/create-link';
-import { getLinks } from './routes/get-links';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipant } from './routes/get-participant';
 
 const app = fastify()
 
@@ -19,9 +24,15 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
+app.register(updateTrip)
 app.register(confirmTrip)
+app.register(getTripDetails)
 
 app.register(confirmParticipant)
+app.register(getParticipants)
+app.register(getParticipant)
+
+app.register(createInvite)
 
 app.register(createActivity)
 app.register(getActivities)
