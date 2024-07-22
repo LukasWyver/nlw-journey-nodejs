cf1c23b1c1d43fbbf4cf874be43cb87c9f783697
lukaswyver <lucas_sin5@hotmail.com>
2024-07-21 22:30:24 -0300

    feat: create route delete trip

---
+++ src/routes/delete-trip.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ClientError } from '../errors/client-error';
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+export async function updateTrip(app: FastifyInstance){
+  app.withTypeProvider<ZodTypeProvider>().delete('/trips/:tripId', {
+    schema: {
+      params: z.object({
+        tripId: z.string().uuid(),
+      })
+    }
+  }, async (request) => {
+    const { tripId } = request.params
+
+    const trip = await prisma.trip.findUnique({
+      where: { id: tripId }
+    })
+
+    if(!trip){
+      throw new ClientError('Trip not found')
+    }
+
+    await prisma.trip.delete({
+      where: { id: tripId },
+    })
+
+    return { tripId: trip.id }
+  })
+}
